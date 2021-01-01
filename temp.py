477dbd013cf9a99e38ce85c859a9d7dff3b26c04
karticr <cleverkriss@gmail.com>
2021-01-02 05:24:26 +0530

    loop test ?

---
+++ temp.py
@@ -0,0 +1,6 @@
+for i  in range(0,28, 10):
+    if(i == 0):
+        print("id=> start:{}, end:{} || val=> start:{}, end:{}".format(i, i+2, i+2, i+18))
+    elif(i >18):
+        print("id=> start:{}, end:{} || val=> start:{}, end:{}".format(i+8, i+10, i+10, i+18))
+
