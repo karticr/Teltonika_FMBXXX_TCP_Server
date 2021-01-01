3eea44cf6edacc49ab6dcec99856c153dc44adb3
karticr <cleverkriss@gmail.com>
2021-01-01 11:22:34 +0530

    test prgm

---
+++ test.py
@@ -0,0 +1,60 @@
+data = b'00000000000001bc080900000176bc2c30f1002fbf481707c6c03a002900991100000009080100020103000400b300b4003200330001480118000000000176bc2b4691002fbf481707c6c03a002900991000000009080100020103000400b300b4003200330001480118000000000176bc2a5c35002fbf481707c6c03a002900991000000009080100020103000400b300b4003200330001480119000000000176bc2971d5002fbf481707c6c03a002900990f00000009080100020103000400b300b4003200330001480119000000000176bc288775002fbf481707c6c03a002900990e00000009080100020103000400b300b4003200330001480119000000000176bc279d15002fbf481707c6c03a002900991100000009080100020103000400b300b4003200330001480119000000000176bc26b2b5002fbf481707c6c03a002900990f00000009080100020103000400b300b4003200330001480119000000000176bc25c859002fbf481707c6c03a002900990f00000009080100020103000400b300b4003200330001480119000000000176bc24ddf9002fbf481707c6c03a002900991000000009080100020103000400b300b40032003300014801190000090000cb7f'
+import binascii
+from binascii import unhexlify
+def decodeVars(data):
+    codecid   = int(data[16:18], 16)
+    record    = int(data[18:20], 16)
+    timestamp = int(data[20:36], 16)
+    lon       = int(data[38:46], 16)
+    lat       = int(data[46:54], 16)
+    alt       = int(data[54:58], 16)
+
+    vars = {
+        "codec" : codecid,
+        "novars": record,
+        "timestamp": timestamp,
+        "gps":{"lon": lon, "lat": lat},
+        "alt": alt
+    }
+    return vars
+
+
+
+
+
+
+
+
+
+
+
+def encoder(command):
+    base_hex = "".join("{:02x}".format(ord(c)) for c in command)
+    print(len(base_hex))
+    beg = 0
+    beg = beg.to_bytes(4, byteorder = 'big')
+
+    print(beg)
+    print(id(binascii.a2b_hex(base_hex)).to_bytes(28, byteorder = 'big'))
+    return base_hex
+
+
+
+
+
+
+# print(decodeVars(data))
+
+
+# td = "00 00 00 00 00 00 00 16 0C 01 05 00 00 00 0E 73 65 74 64 69 67 6f 75 74 20 31 20 36 30 01 00 00 B3 3E"
+# td = td.replace(" ", "")
+
+# print(binascii.a2b_hex(td))
+# print(unhexlify(td))
+# print(bytes.fromhex(td))
+# print(td.decode('hex'))
+
+
+
+# a = "setdigout 1 60"
+# print(encoder(a))
