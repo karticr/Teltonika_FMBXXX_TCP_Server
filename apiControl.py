7a92b8b29086eec978347e4e21ed34ce7d5446b9
karticr <cleverkriss@gmail.com>
2021-01-03 02:46:48 +0530

    avl init

--- apiControl.py
+++ apiControl.py
@@ -10,16 +10,15 @@ class postRequest():
         res = requests.post(url, data=data)
         return res
 
+    def avlToPostData(self, avl):
+        return
+
     def idToAvl(self, data):
-        pass
-
-
-if __name__ == "__main__":
-    data = {
-            "deviceId": "fmb_640_1",
+        format = {
+            "deviceId": data['imei'],
             "nmea": {
-                "lat": "13.0465650",
-                "long": "80.1064433"
+                "lat": data['lat']/10000000,
+                "long": data['lon']/10000000
             },
             "inputs":{
                 "temp1": 20,
@@ -32,6 +31,46 @@ if __name__ == "__main__":
                 "buzzer":0
             }
         }
-    a   = postRequest()
-    url = "https://api.skymarinealert.co.uk/boats/endpoint"
-    print(a.post(url, data))
+        return format
+
+
+if __name__ == "__main__":
+    data = {    
+                'imei': '352093081429150',
+                'sys_time': '03/01/2021 02:31:12',
+                'codecid': 8,
+                'no_record_i': 8,
+                'no_record_e': 8,
+                'crc-16': 54268,
+                'd_time_unix': 1609621190808,
+                'd_time_local': '2021-01-03 02:29:50',
+                'priority': 2,
+                'lon': 801065150,
+                'lat': 130466366,
+                'alt': 21,
+                'angle': 289,
+                'satellites': 10,
+                'speed': 0,
+                'io_data': {
+                    'n1': {
+                        1: 0,
+                        2: 1,
+                        3: 0,
+                        4: 0,
+                        179: 0,
+                        180: 1,
+                        50: 0,
+                        51: 0
+                    },
+                    'n2': {
+                        72: 277
+                    }
+                }
+            }
+
+    
+    a     = postRequest()
+    ready = a.idToAvl(data)
+    print(ready)
+    # url = "https://api.skymarinealert.co.uk/boats/endpoint"
+    # print(a.post(url, data))
