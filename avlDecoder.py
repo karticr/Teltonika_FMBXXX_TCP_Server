75a9e2cc6df28c298005971c0a96ac50011a7e14
karticr <cleverkriss@gmail.com>
2021-01-02 07:05:45 +0530

    unix to local time

--- avlDecoder.py
+++ avlDecoder.py
@@ -1,6 +1,9 @@
+import datetime
 
 class avlDecoder():
     def __init__(self):
         self.data = []
     
-    
+    def unixtoLocal(self, unix_time):
+        time = datetime.datetime.fromtimestamp(unix_time/1000)
+        return f"{time:%Y-%m-%d %H:%M:%S}"
