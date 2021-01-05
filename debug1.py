9b3cdbb2b21bc7f2a3b83e21bbcea38981333edd
karticr <cleverkriss@gmail.com>
2021-01-05 10:43:08 +0530

    testing

--- debug1.py
+++ debug1.py
@@ -1,6 +1,7 @@
 import binascii
 import libscrc
 import crc16
+import sys
 # a = "getinfo"
 # data = binascii.hexlify(a.encode('utf-8'))
 
@@ -17,9 +18,15 @@ import crc16
 
 # print(int(data, 16))
 
-crc_data    = "0C010500000007676574696e666f01"
-crc_encoded = bytes.fromhex(crc_data)
-crc16 = libscrc.ibm(crc_encoded)
-print(crc16)
-print(crc16.to_bytes(4, byteorder='big').hex())
+# crc_data    = "0C010500000007676574696e666f01"
+# crc_encoded = bytes.fromhex(crc_data)
+# crc16 = libscrc.ibm(crc_encoded)
+# print(crc16)
+# print(crc16.to_bytes(4, byteorder='big').hex())
 
+
+data = int("676574696e666f", 16)
+print(data)
+aa = data.to_bytes(7, byteorder='big')
+print(aa)
+sys.stdout.buffer.write(aa)
