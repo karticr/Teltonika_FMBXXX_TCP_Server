af86eeda417105e490d99d46eb012dd6f28f30e9
karticr <cleverkriss@gmail.com>
2021-01-07 10:19:56 +0530

    connection control for quicker updates

---
+++ connectionControl.py
@@ -0,0 +1,15 @@
+
+class connControl():
+    def _init__():
+        self.active_connections = {}
+    
+    def addNew(self, connection):
+        self.
+
+    def removeConnection(self,connection):
+        pass
+
+    def sendCommand(self, connection, command):
+        pass
+    
+    
