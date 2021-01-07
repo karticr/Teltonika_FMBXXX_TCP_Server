7913185eb1d4981aebed14e4341ad884b2f63ca2
karticr <cleverkriss@gmail.com>
2021-01-07 19:00:43 +0530

    post requester init

--- connectionControl.py
+++ connectionControl.py
@@ -1,15 +1,25 @@
+import traceback
+from time import sleep
+from threading import Thread
 
 class connControl:
     def __init__(self):
         self.active_connections = {}
-    
+
     def addNew(self, connection):
         imei = connection['imei']
         conn = connection['conn']
+        print("added new connection to post control")
         self.active_connections[imei] = conn
 
-    def removeConnection(self,connection):
-        pass
+    def removeConnection(self,imei):
+        try:
+            self.active_connections.pop(imei)
+            return True
+        except Exception as e:
+            print(e)
+            print(traceback.format_exc())
+            return False
 
     def sendCommand(self, connection, command):
         pass
@@ -17,10 +27,15 @@ class connControl:
     def getActiveConnections(self):
         return self.active_connections
 
-
+    def postRequester(self):
+        while True:
+            print("here post requester")
+            sleep(5)
 class test:
     def yolo(self):
         return yolo
+
+
 
 if __name__ == '__main__':
     a = connControl()
@@ -29,7 +44,18 @@ if __name__ == '__main__':
         "imei":"1234567",
         "conn": b
     }
+
+    a.addNew(data)
+    print(a.getActiveConnections())
+    data = {
+        "imei":"12345632",
+        "conn": b
+    }
+
     a.addNew(data)
 
+    print(a.getActiveConnections())
+
+    a.removeConnection("12345632")
     print(a.getActiveConnections())
     pass
