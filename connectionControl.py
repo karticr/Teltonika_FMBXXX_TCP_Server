b1d8dfdeb5f45460acf961b6d22f11c27d903af9
karticr <cleverkriss@gmail.com>
2021-01-07 10:29:01 +0530

    tests

--- connectionControl.py
+++ connectionControl.py
@@ -1,10 +1,12 @@
 
-class connControl():
-    def _init__():
+class connControl:
+    def __init__(self):
         self.active_connections = {}
     
     def addNew(self, connection):
-        self.active_connections[connection['imei']] = connection['conn']
+        imei = connection['imei']
+        conn = connection['conn']
+        self.active_connections[imei] = conn
 
     def removeConnection(self,connection):
         pass
@@ -16,5 +18,18 @@ class connControl():
         return self.active_connections
 
 
+class test:
+    def yolo(self):
+        return yolo
+
 if __name__ == '__main__':
+    a = connControl()
+    b = test()
+    data = {
+        "imei":"1234567",
+        "conn": b
+    }
+    a.addNew(data)
+
+    print(a.getActiveConnections())
     pass
